622b87ecd91934bc6ea6a38dc87d76559829acf9
viniciuschiele <vinicius.chiele@gmail.com>
2015-07-09 18:12:44 +0100

    Implemented new unit tests

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -27,3 +27,46 @@ class TestView(TestCase):
 
         response = self.client.post('/repos', data=json.dumps(data), content_type='application/json')
         assert response.status_code == 409
+
+    def test_delete_repo(self):
+        data = dict(owner_id=1, name='test')
+        response = self.client.post('/repos', data=json.dumps(data), content_type='application/json')
+
+        repo = json.loads(response.get_data(as_text=True))
+
+        response = self.client.delete('/repos/' + str(repo.get('id')))
+        assert response.status_code == 204
+
+    def test_delete_not_existing_repo(self):
+        response = self.client.delete('/repos/123123')
+        assert response.status_code == 404
+
+    def test_get_repo(self):
+        data = dict(owner_id=1, name='test')
+        response = self.client.post('/repos', data=json.dumps(data), content_type='application/json')
+        repo = json.loads(response.get_data(as_text=True))
+
+        response = self.client.get('/repos/' + str(repo.get('id')))
+
+        assert response.status_code == 200
+
+        repo = json.loads(response.get_data(as_text=True))
+
+        assert repo.get('name') == 'test'
+
+    def test_get_not_existing_repo(self):
+        response = self.client.get('/repos/2313231')
+
+        assert response.status_code == 404
+
+    def test_list_repositories(self):
+        data = dict(owner_id=1, name='test')
+        self.client.post('/repos', data=json.dumps(data), content_type='application/json')
+
+        response = self.client.get('/repos')
+
+        assert response.status_code == 200
+
+        repositories = json.loads(response.get_data(as_text=True)).get('repositories')
+
+        assert len(repositories) == 1
